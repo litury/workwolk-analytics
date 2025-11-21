d40c37a45c9e489234212a29e8a7980fa16c6d47
Litvinov Y. <litury@icloud.com>
2025-11-21 20:56:08 +0500

    refactor (orm): удалил Prisma ORM и зависимости

--- prisma/migrations/20251115164355_make_telegram_id_optional/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `hh_user_id` on table `users` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "telegram_id" DROP NOT NULL,
-ALTER COLUMN "hh_user_id" SET NOT NULL;
