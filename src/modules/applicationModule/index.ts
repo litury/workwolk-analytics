985dd91767bb5f8cf0b1902aa5020c85fa023a2f
Litvinov Y. <litury@icloud.com>
2026-01-23 15:58:41 +0500

    refactor (modules): удалил модули пользователей, резюме и откликов

--- src/modules/applicationModule/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Application Module Index
- *
- * Следует соглашению proj-str.md - индексный файл для экспорта
- */
-
-export { ApplicationRepository, applicationRepository } from './applicationRepository';
-export { ApplicationService, applicationService } from './applicationService';
-export { applicationRoutes } from './applicationRoutes';
--- src/modules/resumeModule/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Resume Module Index
- *
- * Следует соглашению proj-str.md - индексный файл для экспорта
- */
-
-export { ResumeRepository, resumeRepository } from './resumeRepository';
-export { ResumeService, resumeService } from './resumeService';
-export { resumeRoutes } from './resumeRoutes';
